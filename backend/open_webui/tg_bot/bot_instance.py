39cd66ba9c131c86cdaf977aecd2ac5ac0e076b1
Denys <denis06699@gmail.com>
2025-06-18 14:20:11 +0000

    Fixing errors when variables is not set

--- backend/open_webui/tg_bot/bot_instance.py
+++ backend/open_webui/tg_bot/bot_instance.py
@@ -1,4 +1,14 @@
 from aiogram import Bot
 from open_webui.env import TG_BOT_TOKEN
+import logging
 
-bot = Bot(token=TG_BOT_TOKEN)
+try:
+    if not TG_BOT_TOKEN:
+        bot = None
+        logging.warning("TG_BOT_TOKEN is not set. Bot will not be initialized.")
+    bot = Bot(token=TG_BOT_TOKEN)
+    
+except ValueError as e:
+    logging.warning(f"Error initi: {e}")
+except Exception as e:
+    logging.warning(f"Произошла ошибка при инициализации бота: {e}")
